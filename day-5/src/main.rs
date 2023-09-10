7e6253409f933492af8795d1bde3caed0fe9b7db
Koen Klinkers <k.klinkers@gmail.com>
2023-09-10 23:14:40 +0200

    something is now working yet

---
+++ day-5/src/main.rs
@@ -0,0 +1,91 @@
+use std::fs;
+
+const VOWELS: [char; 5] = ['a', 'e', 'i', 'u', 'o'];
+
+fn main() {
+    let input_file = "./input/input.txt".to_owned();
+    let contents = fs::read_to_string(input_file).expect("Can't read file");
+    let answer = run_1(contents.clone());
+    println!("{}", answer);
+    let answer = run_2(contents.clone());
+    println!("{}", answer);
+}
+
+fn run_1(contents: String) -> usize {
+    contents.split("\n").filter(|x| is_nice(*x)).count()
+}
+
+fn run_2(contents: String) -> usize {
+    contents.split("\n").filter(|x| is_nicer(*x)).count()
+}
+fn is_nice(input: &str) -> bool {
+    if input.chars().filter(|x| VOWELS.contains(x)).count() < 3 {
+        return false;
+    }
+    let mut doubles = false;
+    let iter = input.chars().collect::<Vec<char>>();
+    for wind in iter.windows(2) {
+        match (wind[0], wind[1]) {
+            (a, b) if a == b => doubles = true,
+            ('a', 'b') => return false,
+            ('c', 'd') => return false,
+            ('p', 'q') => return false,
+            ('x', 'y') => return false,
+            _ => {}
+        }
+    }
+    return doubles;
+}
+fn is_nicer(input: &str) -> bool {
+    let iter = input.chars().collect::<Vec<char>>();
+    let mut pass = false;
+    for wind in iter.windows(3) {
+        if wind[0] == wind[2] {
+            pass = true;
+            break;
+        }
+    }
+
+    if !pass {
+        return false;
+    }
+    let mut pair: Vec<(char, char)> = Vec::new();
+    for i in 0..(iter.len() - 1) {
+        if iter[i] == iter[i + 1] {
+            if i != 0 && iter[i - 1] == iter[i] {
+                continue;
+            }
+            if i + 2 > iter.len() && iter[i + 1] == iter[i + 2] {
+                continue;
+            }
+        }
+        pair.push((iter[i], iter[i + 1]));
+    }
+
+    let pre_len = pair.len();
+    pair.sort();
+    pair.dedup();
+    pre_len != pair.len()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    #[test]
+    fn test_day5_1() {
+        assert_eq!(is_nice("ugknbfddgicrmopn"), true);
+        assert_eq!(is_nice("aaa"), true);
+        assert_eq!(is_nice("jchzalrnumimnmhp"), false);
+        assert_eq!(is_nice("haegwjzuvuyypxyu"), false);
+        assert_eq!(is_nice("dvszwmarrgswjxmb"), false);
+    }
+    #[test]
+    fn test_day5_2() {
+        assert_eq!(is_nicer("qjhvhtzxzqqjkmpb"), true);
+        assert_eq!(is_nicer("xxyxx"), true);
+        assert_eq!(is_nicer("uurcxstgmygtbstg"), false);
+        assert_eq!(is_nicer("haegwjzuvuyypxyu"), false);
+        assert_eq!(is_nicer("ieodomkazucvgmuy"), false);
+    }
+}
